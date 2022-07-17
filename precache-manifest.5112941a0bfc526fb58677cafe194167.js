918137d4bd1d2eb6ce2c2041e7cb5d564d0c4183
vivek2502 <vivekgupta.mailme@gmail.com>
2022-07-17 16:39:57 +0530

    Updates

---
+++ precache-manifest.5112941a0bfc526fb58677cafe194167.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f0ddd7787b2084cc7b197bf28c6a07d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f239f27ff2c1493ee0f9",
+    "url": "./static/css/2.80dce9aa.chunk.css"
+  },
+  {
+    "revision": "2eed967422f9b5da20df",
+    "url": "./static/css/main.18de8aef.chunk.css"
+  },
+  {
+    "revision": "f239f27ff2c1493ee0f9",
+    "url": "./static/js/2.c1f06fb6.chunk.js"
+  },
+  {
+    "revision": "30545a24812bc2557c4d652dede4cf2a",
+    "url": "./static/js/2.c1f06fb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2eed967422f9b5da20df",
+    "url": "./static/js/main.ca42a568.chunk.js"
+  },
+  {
+    "revision": "35eff47ceeca097ad7eb",
+    "url": "./static/js/runtime-main.2bd40bad.js"
+  },
+  {
+    "revision": "d6cbd8e6d17d679dd3cd7fe3884ac8bb",
+    "url": "./static/media/photo.d6cbd8e6.png"
+  }
+]);
